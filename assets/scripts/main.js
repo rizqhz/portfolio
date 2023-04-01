2bb2800e1ebeae56d324be33ba9acd091de75715
Ghozali <riz.ghz@gmail.com>
2023-04-01 14:56:09 +0700

    Update Portfolio

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+let path = 'assets/images/profile.jpg'
+let buffer = document.querySelector('#foto-profil');
+buffer.setAttribute('src', path);
+buffer.setAttribute('alt', 'foto profil orang');
+
+let data = [];
